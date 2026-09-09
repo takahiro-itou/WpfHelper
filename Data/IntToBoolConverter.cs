56636f3f6ff2db559901a5950ddb4ee0b203c919
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-09 16:53:29 +0900

    数値のブールのコンバータを実装

--- Data/IntToBoolConverter.cs
+++ Data/IntToBoolConverter.cs
@@ -12,11 +12,61 @@
 **                                                                      **
 *************************************************************************/
 
+using   System.Globalization;
+using   System.Windows.Data;
+
 
 namespace  WpfHelper.Data  {
 
-public  class  IntToBoolConverter
+//========================================================================
+//
+//    IntToBoolConverter  class
+//
+
+[ValueConversion(typeof(int), typeof(bool))]
+public  class  IntToBoolConverter : IValueConverter
 {
+
+//----------------------------------------------------------------
+/**   数値をブールに変換する。
+**
+**    ビューモデルからビュー（画面）への対応。
+**/
+public  object
+Convert(
+        object      value,
+        Type        targetType,
+        object      parameter,
+        CultureInfo culture)
+{
+    if ( value is int intValue && parameter != null ) {
+        int paramValue = int.Parse(parameter.ToString());
+        return ( intValue == paramValue );
+    }
+    return ( false );
+}
+
+
+//----------------------------------------------------------------
+/**   ブールを数値に変換する。
+**
+**    ビュー（画面）からビューモデルへの対応。
+**  ラジオボタン等がクリックされたときに、
+**  選択されたボタンに応じて数値をビューモデルに書き戻す。
+**/
+public  object
+ConvertBack(
+        object      value,
+        Type        targetType,
+        object      parameter,
+        CultureInfo culture)
+{
+    if ( value is bool boolValue && boolValue && parameter != null ) {
+        return  int.Parse(parameter.ToString());
+    }
+
+    return ( System.Windows.Data.Binding.DoNothing );
+}
 
 
 }   //  End of class  IntToBoolConverter
