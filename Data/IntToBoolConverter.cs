c37cdd874588c7684149dea1332251b6dc067bc5
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-09 16:41:55 +0900

    空のクラスを新規作成

---
+++ Data/IntToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature-unix     -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   WPF  Helper  Library   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  WpfHelper.Data  {
+
+public  class  IntToBoolConverter
+{
+
+
+}   //  End of class  IntToBoolConverter
+
+}   //  End of namespace  WpfHelper.Data
