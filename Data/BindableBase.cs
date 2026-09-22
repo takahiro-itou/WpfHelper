659ea84b96f03297f750a0b3d7043bc655efd142
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-22 09:42:14 +0900

    空のクラスを新規作成

---
+++ Data/BindableBase.cs
@@ -0,0 +1,31 @@
+﻿//  -*-  coding: utf-8-with-signature-unix     -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   WPF  Helper  Library   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using   System.ComponentModel;
+using   System.Runtime.CompilerServices;
+
+
+namespace  WpfHelper.Data  {
+
+//========================================================================
+//
+//    BindableBase  class
+//
+
+public  class  BindableBae : INotifyPropertyChanged
+{
+
+}   //  End of class  BindableBase
+
+}   //  End of namespace  WpfHelper.Data
